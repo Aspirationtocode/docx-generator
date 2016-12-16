e71271aacbe880743ecaed5d01d43e21beae7256
aspirationtocode <aspiretocode@gmail.com>
2016-12-16 22:50:44 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const fs = require('fs');
+const generateDocx = require('generate-docx');
+const bodyParser = require('body-parser')
+
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,3 +20,37 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/generate', (req, res) => {
+  const data = req.body;
+  const options = {
+    template: {
+      filePath: 'public/input.docx',
+      data: {
+        student_firstname: data.student_firstname.slice(0, 1),
+        student_lastname: data.student_lastname,
+        student_patronym: data.student_patronym.slice(0, 1),
+        group: data.group,
+        student_code: data.student_code,
+        professor_firstname: data.professor_firstname.slice(0, 1),
+        professor_lastname: data.professor_lastname,
+        professor_patronym: data.professor_patronym.slice(0, 1),
+        year: data.year,
+        subject: data.subject,
+        topic: data.topic,
+      }
+    }
+  }
+  generateDocx(options, (error, buf) => {
+    if (error) {
+      console.error(error)
+    } else {
+      fs.writeFileSync('public/output.docx', buf)
+      console.log('File written')
+    }
+  })
+  res.render('pages/index');
+})
